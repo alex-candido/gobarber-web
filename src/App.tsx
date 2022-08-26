641fa8c6b404d0bad57b20d1e0ab0e712430cce1
Alex Cândido <alex.candido.tec@gmail.com>
2022-08-25 21:11:21 -0300

    pagina de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import SignIn from './pages/SignIn';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
-  return <div>Hello Word</div>;
+  return (
+    <>
+      <SignIn />
+      <GlobalStyle />
+    </>
+  );
 };
 
 export default App;
