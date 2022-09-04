022b8ec123c9b68ae01a10aba80d20f01674074d
Alex Cândido <alex.candido.tec@gmail.com>
2022-09-03 23:16:06 -0300

    rotas autenticadas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import ProtectedRoute, { ProtectedRouteProps } from './Route';
+
+const defaultProtectedRouteProps: Omit<ProtectedRouteProps, 'component'> = {
+  isPrivate: false,
+  authenticationPath: '/dashboard',
+};
 
 const Router: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute
+            {...defaultProtectedRouteProps}
+            component={<SignIn />}
+          />
+        }
+      />
+      <Route
+        path="/signup"
+        element={
+          <ProtectedRoute
+            {...defaultProtectedRouteProps}
+            component={<SignUp />}
+          />
+        }
+      />
+
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 };
